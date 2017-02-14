39747fd243d932cd8cd73827d08324363b4c6377
Kakani Padmaja -X (kpadmaja - INFOSYS LIMITED at Cisco) <kpadmaja@partnet.cisco.com>
2017-02-14 17:06:36 +0530

    second commit

--- src/main/java/com/infosys/chatbot/CrucibleIntegration.java
+++ src/main/java/com/infosys/chatbot/CrucibleIntegration.java
@@ -2,17 +2,18 @@ package com.infosys.chatbot;
 
 
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 public class CrucibleIntegration {
 	public static void main(String[] args) {
 		try{
-			URL url = new URL("http://localhost:8060/cru/CR-1#c2");
+			URL url = new URL("https://github.com/Padmaja7/newApp/commits/master");
 			URLConnection con = url.openConnection();
 			InputStream in = con.getInputStream();
 			String encoding = con.getContentType();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
@@ -21,7 +22,12 @@ public class CrucibleIntegration {
 			else
 				encoding = "UTF-8";
 			String body = IOUtils.toString(in, encoding);
-			System.out.println(body);
+			//System.out.println(body);
+			Document doc = Jsoup.connect("https://github.com/Padmaja7/newApp/commits/master").get();
+			//System.out.println(doc.select(".commit-title a"));
+			System.out.println("Commit Information :"+ doc.getElementsByClass("message").text());
+			System.out.println("Committed by :"+ doc.getElementsByClass("user-mention").text());
+			System.out.println("Committed at :"+ doc.getElementsByClass("commit-group-title").text().split("on")[1]);
 		}
 		catch (Exception e) {
 			// TODO: handle exception
